8f5d031377875bdad65154d9653bebc579321d88
jaredjames2020 <jaredjames2020@gmail.com>
2020-06-01 19:05:50 -0400

    commit

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -44,7 +44,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas
         onMouseMove={this.handleMouseMove}
         onClick={this.handleOnClick}
-        handleOnKeyPress={this.handleOnKeyPress}
+        OnKeyPress={this.handleOnKeyPress}
         width='900'
         height='600'
         tabIndex="0">
